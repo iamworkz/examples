090fe067785ba1966f9a0508669830f0869816f1
Haiko van der Schaaf <hvdschaaf@gmail.com>
2025-04-26 16:17:15 +0200

    init Spring Security Keycloak example

---
+++ spring-security-keycloak/src/main/java/com/iamworkz/spring_security_keycloak/SpringSecurityKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.iamworkz.spring_security_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityKeycloakApplication.class, args);
+	}
+
+}
